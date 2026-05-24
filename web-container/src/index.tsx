20df8eb7ff0d514a551706e7ea4ca6fad4e33eb9
송영진 <youngjin.song@ltree.co.kr>
2026-05-24 13:25:43 +0900

    MSW (Mock Service Worker) 추가

--- web-container/src/index.tsx
+++ web-container/src/index.tsx
@@ -9,13 +9,23 @@ import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 import '@styles/index.css';
 
+async function enableMocking() {
+  if (import.meta.env.MODE !== 'development') {
+    return;
+  }
+  const { worker } = await import('./mocks/browser');
+  return worker.start();
+}
+
 const queryClient = new QueryClient();
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
-  </StrictMode>
-);
+enableMocking().then(() => {
+  createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+      <QueryClientProvider client={queryClient}>
+        <App />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    </StrictMode>
+  );
+});
