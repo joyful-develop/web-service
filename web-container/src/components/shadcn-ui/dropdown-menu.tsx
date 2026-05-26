48724b3ff7193d2ad7c54780cfd5a7a9f7345f4a
송영진 <youngjin.song@ltree.co.kr>
2026-05-26 19:33:14 +0900

    lint:fix 실행 결과 적용

--- web-container/src/components/shadcn-ui/dropdown-menu.tsx
+++ web-container/src/components/shadcn-ui/dropdown-menu.tsx
@@ -86,12 +86,10 @@ function DropdownMenuCheckboxItem({
         className
       )}
       checked={checked}
-      {...props}
-    >
+      {...props}>
       <span
         className='pointer-events-none absolute right-2 flex items-center justify-center'
-        data-slot='dropdown-menu-checkbox-item-indicator'
-      >
+        data-slot='dropdown-menu-checkbox-item-indicator'>
         <DropdownMenuPrimitive.ItemIndicator>
           <CheckIcon />
         </DropdownMenuPrimitive.ItemIndicator>
@@ -121,12 +119,10 @@ function DropdownMenuRadioItem({
         "focus:bg-accent focus:text-accent-foreground focus:**:text-accent-foreground relative flex cursor-default items-center gap-1.5 rounded-md py-1 pr-8 pl-1.5 text-sm outline-hidden select-none data-disabled:pointer-events-none data-disabled:opacity-50 data-inset:pl-7 [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4",
         className
       )}
-      {...props}
-    >
+      {...props}>
       <span
         className='pointer-events-none absolute right-2 flex items-center justify-center'
-        data-slot='dropdown-menu-radio-item-indicator'
-      >
+        data-slot='dropdown-menu-radio-item-indicator'>
         <DropdownMenuPrimitive.ItemIndicator>
           <CheckIcon />
         </DropdownMenuPrimitive.ItemIndicator>
@@ -196,8 +192,7 @@ function DropdownMenuSubTrigger({
         "focus:bg-accent focus:text-accent-foreground not-data-[variant=destructive]:focus:**:text-accent-foreground data-open:bg-accent data-open:text-accent-foreground flex cursor-default items-center gap-1.5 rounded-md px-1.5 py-1 text-sm outline-hidden select-none data-inset:pl-7 [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4",
         className
       )}
-      {...props}
-    >
+      {...props}>
       {children}
       <ChevronRightIcon className='ml-auto' />
     </DropdownMenuPrimitive.SubTrigger>
